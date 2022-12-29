05e78f1a1c4734d73b9e4351bf003e981d5e1f60
camunozn <camunozn89@gmail.com>
2022-12-29 15:06:25 -0500

    added html estructure

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,43 @@ function App() {
 
   return (
     <div className="App">
-      <header>
+      <header className="header">
         <div className="app-logo"></div>
-        <nav className="main-nav"></nav>
+        <nav className="main-nav">
+          <ul className="main-nav-list">
+            <li className="main-nav-link">
+              <a href="">Home</a>
+            </li>
+            <li className="main-nav-link">
+              <a href="">Details</a>
+            </li>
+            <li className="main-nav-link">
+              <a href="">Options</a>
+            </li>
+          </ul>
+        </nav>
         <div className="menu-mobile"></div>
       </header>
       <main>
-        <section className="section-summary"></section>
-        <section className="section-details"></section>
-        <section className="section-options"></section>
+        <section className="section-summary">
+          <div className="summary-city">Quito, Ecuador</div>
+          <div className="summary-degrees">Icono + 24ºC</div>
+          <div className="summary-climate">Sunny H: 28º L:15º</div>
+        </section>
+        <section className="section-details">
+          <div className="details-box detail--1">Precipitation</div>
+          <div className="details-box detail--2">UV index</div>
+          <div className="details-box detail--3">Wind speed</div>
+          <div className="details-box detail--4">Pressure</div>
+        </section>
+        <section className="section-options">
+          <div className="options-location">Location</div>
+          <div className="options-units">Units</div>
+        </section>
       </main>
+      <footer className="footer">
+        <div className="footer-copyright">Copyright</div>
+      </footer>
     </div>
   );
 }
